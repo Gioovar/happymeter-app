9cf2ce346f6afab9e24b6d0726e04e0255f3580a
Giovanni Vargas Saucedo <gioovar@MacBook-Pro-de-Giovanni.local>
2026-01-13 22:31:32 -0600

    fix: increase server actions body size limit to 4mb

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,11 @@ const nextConfig = {
             },
         ],
     },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '4mb',
+        },
+    },
 };
 
 const pwaConfig = withPWA({
