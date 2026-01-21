951b9c732fd4b2d6d31b45ccb46e9be48e365c43
Giovanni Vargas Saucedo <gioovar@MacBook-Pro-de-Giovanni.local>
2026-01-20 23:11:47 -0600

    style: fix switch toggle visual alignment in RecoverySettings

--- src/components/RecoverySettings.tsx
+++ src/components/RecoverySettings.tsx
@@ -89,7 +89,7 @@ export default function RecoverySettings({ initialConfig, onSave }: { initialCon
                 </div>
                 <label className="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" checked={config.enabled} onChange={(e) => setConfig({ ...config, enabled: e.target.checked })} className="sr-only peer" />
-                    <div className="w-11 h-6 bg-gray-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-pink-500"></div>
+                    <div className="relative w-11 h-6 bg-gray-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-pink-500"></div>
                 </label>
             </div>
 
@@ -129,7 +129,7 @@ export default function RecoverySettings({ initialConfig, onSave }: { initialCon
                                         onChange={(e) => updateTier(activeTab, 'enabled', e.target.checked)}
                                         className="sr-only peer"
                                     />
-                                    <div className="w-9 h-5 bg-gray-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-violet-500"></div>
+                                    <div className="relative w-9 h-5 bg-gray-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-violet-500"></div>
                                 </label>
                             </div>
 
